038871004dec68a578181c66b7d1622a9197b29f
trebla01 <treblanehc95@gmail.com>
2018-07-18 18:04:04 -0700

    first update. Added in graph class and main

--- graph.py
+++ graph.py
@@ -1,8 +1,13 @@
-#graph with male and female nodes
+import random
 
 class Full_Graph:
     def __init__(self):
+        # keep a graph of users and all potential matches
         self.graph = {}
+        # keep a list of all 1 way matches that are made. Note that the format:(personA, personB), personA swiped right on personB, but not vice versa
+        self.half_matches = []
+        # keep a list of all 2 way matches
+        self.matches = []
 
     def add_person(self, new_person):
         self.graph[new_person] = []
@@ -17,15 +22,52 @@ class Full_Graph:
                     self.graph[person].append(new_person)
                     self.graph[new_person].append(person)
 
-    def run_network(self):
+    def simulate_network(self):
+        for user in self.graph:
+            for potential_match in self.graph[user]:
+                r = random.randint(0,100)
+                if r < user.swipe_chance:
+                    if (potential_match, user) in self.half_matches:
+                        self.half_matches.remove((potential_match, user))
+                        self.matches.append((user, potential_match))
+                    else:
+                        self.half_matches.append((user, potential_match))
+
+    def summarize_network(self):
         pass
+
+    def summarize_matches(self):
+        for match in self.half_matches:
+            print("One sided match: ", match)
+        for match in self.matches:
+            print("Full match: ", match)
+
+    def summarize_user_experience(self):
+        for user in self.graph:
+            fail = 0
+            success = 0
+            for match in self.half_matches:
+                if match[0] == user:
+                    fail += 1
+            for match in self.matches:
+                if match[0] == user or match[1] == user
+                    success += 1
+            percent_success = success / (success + fail) * 100
+
 
 # a person has several traits
 # male/female
 # %chance to swipe right, from 0 to 100
 class Person:
-    def __init__(self, gender, swipe_chance):
+    def __init__(self, idx, gender, swipe_chance):
+        self.idx = idx
         self.gender = gender
         self.swipe_chance = swipe_chance
+    def __repr__(self):
+        ret_str = "person " + str(self.idx) + ": " + self.gender
+        return ret_str
+    def __str__(self):
+        ret_str = "person " + str(self.idx) + ": " + self.gender
+        return ret_str
 
 
--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 from graph import *
-import random
-
-
 
 #make 5 males and 5 females and add it to the network
 initial_people = []
-for i in range(5):
-    initial_people.append(Person("male", 70))
-    initial_people.append(Person("female", 30))
+for i in range(10):
+    initial_people.append(Person(i, "male", 70))
+    initial_people.append(Person(i+5, "female", 30))
+
 network = Full_Graph()
 for individual in initial_people:
     network.add_person(individual)
-
-
-for user in network.graph:
-    m_count = 0
-    f_count = 0
-    for potential_match in network.graph[user]:
-        if potential_match.gender == "male":
-            m_count += 1
-        else:
-            f_count += 1
-    print(m_count, f_count)
+network.simulate_network()
+network.summarize_network()
 
